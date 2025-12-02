c5227861b6dd54465b5e1e0505ab0ba95f44a36d
Vasiliy76rus <vedrusclan@ya.ru>
2025-12-02 14:40:07 +0300

    Update terminology: Ancestral Capital to Intergenerational (Ancestral) Capital

--- convert_to_vk_v2.py
+++ convert_to_vk_v2.py
@@ -2,8 +2,8 @@ import json
 import re
 import os
 
-input_path = r"c:\Users\admin\Desktop\Проект РП\Итоги.ipynb"
-output_path = r"c:\Users\admin\Desktop\Проект РП\Итоги_VK.md"
+input_path = r"c:\Users\admin\Desktop\Проект РП\Release\Pack_for_GitHub_and_Media\Итоги.ipynb"
+output_path = r"c:\Users\admin\Desktop\Проект РП\Release\Pack_for_GitHub_and_Media\Итоги_VK.md"
 
 print(f"Reading from: {input_path}")
 
