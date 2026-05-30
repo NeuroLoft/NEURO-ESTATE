51a93672632886fa97e7797333f25c8519d04b9a
NeuroLoft <ecobrainstorming@gmail.com>
2026-05-31 00:57:17 +0300

    Clean up repository, keeping only TEO core

--- push_script.bat
+++
@@ -1,7 +0,0 @@
-cd /d "c:\Users\admin\Desktop\Проект РП\Release"
-echo START > debug.txt
-git config core.editor echo
-git merge --abort
-git pull origin main
-git push origin main > push_log.txt 2>&1
-echo END >> debug.txt
